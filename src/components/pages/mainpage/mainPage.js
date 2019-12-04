71bab4a294a2e5bd5fb4a1653746ec2de3e04acb
Yauhen <zhendos@tut.by>
2019-12-04 08:55:48 +0100

    added functional to button More

--- src/components/pages/mainpage/mainPage.js
+++ src/components/pages/mainpage/mainPage.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
+import {Link} from 'react-router-dom';
 import Header from '../../header';
 import Footer from '../../footer';
 import beans_logo from '../../../media/logo/Beans_logo.svg';
-import beans_logo_dark from '../../../media/logo/Beans_logo_dark.svg';
+
 import './mainPage.sass';
 import classes from './mainPage.module.css';
+import About from '../../about';
 
 export default class MainPage extends Component {
   render(){
@@ -20,39 +22,13 @@ export default class MainPage extends Component {
                 <img className="beanslogo" src={beans_logo} alt="Beans logo"></img>
                 <div className="preview__subtitle">We makes every day full of energy and taste</div>
                 <div className="preview__subtitle">Want to try our beans?</div>
-                <a href="#" className="preview__btn">More</a>
+                <Link to="/coffe-page/" className="preview__btn">More</Link>
               </Col>
             </Row>
           </Container>
           </div>
-          <section className="about">
 
-            <Container>
-              <Row>
-                <Col lg={{size: 6, offset: 3}}>
-                
-                      <div className="title">About Us</div>
-                      <img className="beanslogo" src={beans_logo_dark} alt="Beans logo"></img>
-                      <div className="about__text">
-                          Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
-                          Afraid at highly months do things on at. Situation recommend objection do intention
-                          so questions. As greatly removed calling pleased improve an. Last ask him cold feel
-                          met spot shy want. Children me laughing we prospect answered followed. At it went
-                          is song that held help face.<br/><br/>
-
-                          Now residence dashwoods she excellent you. Shade being under his bed her, Much
-                          read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant
-                          horrible but confined day end marriage. Eagerness furniture set preserved far
-                          recommend. Did even but nor are most gave hope. Secure active living depend son
-                          repair day ladies now.
-                      </div>
-                
-                </Col>
-              </Row>
-            </Container>
-
-          </section>
-
+          <About/>
           <section className={classes.best}>
               <Container>
                   <div className="title">Our best</div>
